dcf771d2da94d8274009a3b1ce0222e88d1e1e51
LosmanDev <losman78@gmail.com>
2020-08-08 18:13:34 -0400

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+const fetchData = async (searchTerm) => {
+  const response = await axios.get(" http://www.omdbapi.com/", {
+    params: {
+      apikey: "1fce14d7",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
+};
+
+let timeoutId;
+const input = document.querySelector("input");
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+input.addEventListener("input", onInput);
