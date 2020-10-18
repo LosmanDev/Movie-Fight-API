e44b564e34be7688dfa759777d635044a83214f6
LosmanDev <losman78@gmail.com>
2020-10-17 21:41:36 -0400

    update

--- index.js
+++ index.js
@@ -11,33 +11,7 @@ const debounce = (func, delay = 1000) => {
   };
 };
 
-const fetchData = async (searchTerm) => {
-  const response = await axios.get(" http://www.omdbapi.com/", {
-    params: {
-      apikey: "1fce14d7",
-      s: searchTerm,
-    },
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
-const onMovieSelect = async (movie) => {
-  const response = await axios.get(" http://www.omdbapi.com/", {
-    params: {
-      apikey: "1fce14d7",
-      i: movie.imdbID,
-    },
-  });
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
-};
-
-createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+const autocompleteConfig = {
   renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     return `
@@ -45,9 +19,78 @@ createAutoComplete({
     ${movie.Title} (${movie.Year})
   `;
   },
+
+  inputValue(movie) {
+    return movie.Title;
+  },
+
+  async fetchData(searchTerm) {
+    const response = await axios.get(" http://www.omdbapi.com/", {
+      params: {
+        apikey: "1fce14d7",
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  },
+};
+
+createAutoComplete({
+  ...autocompleteConfig,
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"), "left");
+  },
+});
+createAutoComplete({
+  ...autocompleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"), "right");
+  },
 });
 
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
+  const response = await axios.get(" http://www.omdbapi.com/", {
+    params: {
+      apikey: "1fce14d7",
+      i: movie.imdbID,
+    },
+  });
+  summaryElement.innerHTML = movieTemplate(response.data);
+
+  if (side === "left") {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {};
+
 const movieTemplate = (movieDetail) => {
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  );
+  const metaScore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+  console.log(metaScore, imdbRating, imdbVotes);
+
   return `
   <article class="media">
   <figure class="media-left">
